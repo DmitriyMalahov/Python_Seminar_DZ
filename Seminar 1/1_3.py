66a0becac4c069868ba94c8e488a97c9a3305158
Dmitriy_Malakhov <lesnichiy1986@gmail.com>
2023-02-02 14:42:34 +0300

    task completed 1_3

--- Seminar 1/1_3.py
+++ Seminar 1/1_3.py
@@ -12,17 +12,13 @@
 n = int(input('Введите шесть чисел номера билета: '))
 
 number1 = n % 10
-number = n // 10
-number2 = number % 10
-number = number // 10
-number3 = number % 10
+number2 = n // 10 % 10
+number3 = n // 100 % 10
 sum1 = number1 + number2 + number3
 
 number4 = n // 1000 % 10
-number = n // 10000
-number5 = number % 10
-number = number // 10
-number6 = number % 10
+number5 = n // 10000 % 10
+number6 = n // 100000 % 10
 sum2 = number4 + number5 + number6
 
 result = 'YES' if (sum1 == sum2) else 'NO'
