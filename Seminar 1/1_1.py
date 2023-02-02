d7bfebe80e831775c8a346af75c604f14c979fe8
Dmitriy_Malakhov <lesnichiy1986@gmail.com>
2023-02-02 14:44:36 +0300

    fixed task 1_1

--- Seminar 1/1_1.py
+++ Seminar 1/1_1.py
@@ -7,9 +7,7 @@
 n = int(input('Введите трехзначное число: '))
 
 number3 = n % 10
-number = n // 10
-number2 = number % 10
-number = number // 10
-number1 = number % 10
+number2 = n // 10 % 10
+number1 = n // 100 % 10
 
 print(f'Сумма цифр трехзначного числа {n} равна: {number1 + number2 + number3}')
