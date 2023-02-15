9efb925e3d7ce740254e5d383ff4b1a0e626c798
Dmitriy Malakhov <lesnichiy1986@gmail.com>
2023-02-15 16:07:47 +0300

    task completed 5_1

--- Seminar_5/5_2.py
+++ Seminar_5/5_2.py
@@ -9,19 +9,17 @@
 # 4
 
 
-sys.getrecursionlimit(15000)
+
 
 def sum_two_numbers(a, b):
     if a == 0:
         return b
-    if b == 0:
-        return a
     if a > 0:
         return sum_two_numbers(a-1,b+1)
-    if b > 0:
-        return sum_two_numbers(a+1,b-1)
+
     
 a = int(input('Введите число а: ')) 
 b = int(input('Введите число b: '))
+
 print(sum_two_numbers(a, b))
 
